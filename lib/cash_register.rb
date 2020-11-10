0a3cbbe0196d068eb8e2f18249598cde2c9f5710
Kimberly Baker <bakerk2010@gmail.com>
2020-11-10 23:52:18 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -26,9 +26,9 @@ class CashRegister
   
   def apply_discount
     if @discount > 0
-       @take_off = (price * discount) / 100
-       @total -= @take_off
-      # @total -= @total * @discount / 100
+       # @take_off = (price * discount) / 100
+       # @total -= @take_off
+       @total -= @total * @discount / 100
       return "After the discount, the total comes to $#{total}."
     else
       return "There is no discount to apply."
